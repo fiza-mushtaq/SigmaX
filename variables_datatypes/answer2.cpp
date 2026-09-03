2f22695dc9fb4f38933c03d91a4b6d57d1a3e5a2
fiza-mushtaq <fizamshtq@gmail.com>
2026-09-03 17:14:48 +0530

    add area calculation for a circle

--- variables_datatypes/answer2.cpp
+++ variables_datatypes/answer2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int pencilCost, penCost, eraserCost;
+    float pencilCost, penCost, eraserCost;
         cout << "Enter cost of pencil: ";
         cin >> pencilCost;         
         cout << "Enter cost of pen: ";      
@@ -10,10 +10,10 @@ int main() {
         cout << "Enter cost of eraser: ";   
         cin >> eraserCost;
 
-    int totalCost = pencilCost + penCost + eraserCost;
+    float totalCost = pencilCost + penCost + eraserCost;
     cout << "Total cost of pencil, pen and eraser is: " << totalCost << endl;
 
-    int costWithTax = totalCost + (totalCost * 0.18);
+    float costWithTax = totalCost + (totalCost * 0.18);
     cout << "Total cost with 18% tax is: " << costWithTax << endl;
 
     return 0;
---
+++ variables_datatypes/answer4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+# define PI 3.14
+
+int main() {
+        float radius = 4;
+        float area = PI * radius * radius;
+        cout << "Area of circle is: " << area << endl;
+    return 0;
+}
