bcabe14f2822867c3a685d29861db60e3c798ac0
fiza-mushtaq <fizamshtq@gmail.com>
2026-09-03 17:29:56 +0530

    Add program to check if a 3-digit number is an Armstrong number

---
+++ conditionalStatements/answer5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int num;
+        cout << "Enter a 3-digit number: ";
+        cin >> num;
+
+        int n = num;
+
+        int digit1 = n % 10; // Extract the last digit
+        n /= 10; // Remove the last digit
+
+        int digit2 = n % 10; // Extract the second digit
+        n /= 10; // Remove the second digit
+
+        int digit3 = n % 10; // Extract the first digit
+        n /= 10; // Remove the first digit
+        
+        int cubeSum = (digit1 * digit1 * digit1) + (digit2 * digit2 * digit2) + (digit3 * digit3 * digit3);
+
+        if(cubeSum == num) {
+            cout << num << " is an Armstrong number." << endl;
+        } else {
+            cout << num << " is not an Armstrong number." << endl;
+        }
+
+    return 0;
+}
