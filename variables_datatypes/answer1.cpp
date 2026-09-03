0b41b81290f6810a533312f1996936539aecf907
fiza-mushtaq <fizamshtq@gmail.com>
2026-09-03 16:57:07 +0530

    Add initial implementation of area calculation for a square

---
+++ variables_datatypes/answer1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int side = 7;
+        int area = side * side;
+
+        cout << "The area of the square is: " << area << endl;
+        
+    return 0;
+}
